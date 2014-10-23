13aebaf9e8a8f0fa4cfd2495b5b0dfca247404b3
Juan Arias <biskuiarias@gmail.com>
2014-10-23 08:12:16 -0600

    Cambis en Rol.

--- BE-COM/UI/GlobalModule.vb
+++ BE-COM/UI/GlobalModule.vb
@@ -3,9 +3,9 @@ Imports EntitiesLayer
 
 Module GlobalModule
 
-    Public gestorPermiso As New Gestor()
+    Public gestorUsuario As New GestorUsuarios()
     Public validaciones As New Validaciones()
-    Public campoCorrecto As Image = Image.FromFile("C:\Users\Douglas\Documents\Soft506\BE-COM\Imagens\Validaciones\Correcto.png")
-    Public campoIncorrecto As Image = Image.FromFile("C:\Users\Douglas\Documents\Soft506\BE-COM\Imagens\Validaciones\Incorrecto.png")
+    Public campoCorrecto As Image = Image.FromFile("C:\Users\JuanManuel\Documents\Soft506\Soft506\BE-COM\Imagens\Validaciones\Correcto.png")
+    Public campoIncorrecto As Image = Image.FromFile("C:\Users\JuanManuel\Documents\Soft506\Soft506\BE-COM\Imagens\Validaciones\Incorrecto.png")
 
 End Module
